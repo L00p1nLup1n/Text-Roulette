cde057ce104528ae31727516b5fab8c15d08e3f4
L00p1nLup1n <lykhoidu@gmail.com>
2024-01-15 01:26:41 +0700

    update 2.2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) throws GameOver {
-        Shotgun gun = new Shotgun(0);
-        gun.welcomeText();
+        Shotgun gun = new Shotgun();
         gun.createPlayers();
+        Shotgun.clearScreen();
+        Shotgun.welcomeText();
         gun.gameLogic();
     }
 }
