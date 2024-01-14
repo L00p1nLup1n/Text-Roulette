0c70b85e89e91cb05d7d58dcc5745fc9c7bcf56d
L00p1nLup1n <lykhoidu@gmail.com>
2024-01-14 17:36:32 +0700

    update 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,4 @@
-import java.util.*;
-
 public class Main {
-    public static Scanner scn = new Scanner(System.in);
-
     public static void main(String[] args) throws GameOver {
         Shotgun gun = new Shotgun(0);
         gun.welcomeText();
